a439dabc872b9b0d9d8300697b8445830238e486
Alexandre Faisst <alefaisst10@gmail.com>
2022-08-30 16:21:27 -0300

    Add HTML, CSS and JS files

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+
+defaultGrid(16);
+
+function defaultGrid(num) {
+    makeRows(num);
+    makeColumns(num);
+}
+
+function makeRows(rowNum) {
+
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+};
+
+function resize(){
+
+  container.replaceChildren();
+
+  let userInput = parseInt(prompt("Enter the number of squares per side: "));
+
+  defaultGrid(userInput);
+}
